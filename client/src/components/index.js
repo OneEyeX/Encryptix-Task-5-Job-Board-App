7b4230440626d144e63dbced8c3d29c23eb926ed
OneEyeX <chedly199@gmail.com>
2024-07-04 05:29:55 +0100

    cleaned code and completed companies pages and added loading animation

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,12 +3,12 @@ import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Header from "./Header";
 import JobCard from "./JobCard";
-// import JobTypes from "./JobTypes";
 import ListBox from "./ListBox";
 import Loading from "./Loading";
 import Navbar from "./Navbar";
 import SignUp from "./SignUp";
 import TextInput from "./TextInput";
+// import JobTypes from "./JobTypes";
 
 export {
     // CompanyCard, CustomButton, Footer, Header, JobCard, JobTypes, ListBox, Loading, Navbar, SignUp, TextInput
