9e2c0d51ce1324d24659a56b9090b427ef021a7c
OneEyeX <chedly199@gmail.com>
2024-07-05 02:38:58 +0100

    completed user create and update fucntionalitiess

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,19 +1,26 @@
 import JWT from "jsonwebtoken";
 
-const userAuth = async (err, req, res, next) => {
+const userAuth = async (req, res, next) => {
     const authHeader = req?.headers?.authorization;
+
     if (!authHeader || !authHeader?.startsWith("Bearer")) {
-        next("Authentication Failed");
+        next("Authentication== failed");
     }
+
     const token = authHeader?.split(" ")[1];
+
     try {
         const userToken = JWT.verify(token, process.env.JWT_SECRET_KEY);
+
         req.body.user = {
             userId: userToken.userId,
-        }
+        };
+
         next();
     } catch (error) {
-        console.log(error.message);
-        next("Authentication Failed");
+        console.log(error);
+        next("Authentication failed");
     }
 };
+
+export default userAuth;
