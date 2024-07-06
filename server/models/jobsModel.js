ba02d69acfed9e6049a75248d96ddbb5bb432650
OneEyeX <chedly199@gmail.com>
2024-07-06 04:55:03 +0100

    completed backend and testing

--- server/models/jobsModel.js
+++ server/models/jobsModel.js
@@ -1,45 +1,19 @@
-// import bcrypt from "bcryptjs";
-// import JWT from "jsonwebtoken";
-import { Schema } from "mongoose";
-// import validator from "validator";
+import mongoose, { Schema } from "mongoose";
 
-const jobSchema = new Schema({
-    company: {
-        type: Schema.Types.ObjectId, ref: "Companies"
+const jobSchema = new mongoose.Schema(
+    {
+        company: { type: Schema.Types.ObjectId, ref: "Companies" },
+        jobTitle: { type: String, required: [true, "Job Title is required"] },
+        jobType: { type: String, required: [true, "Job Type is required"] },
+        location: { type: String, required: [true, "Location is required"] },
+        salary: { type: Number, required: [true, "Salary is required"] },
+        vacancies: { type: Number },
+        experience: { type: Number, default: 0 },
+        detail: [{ desc: { type: String }, requirements: { type: String } }],
+        application: [{ type: Schema.Types.ObjectId, ref: "Users" }],
     },
-    jobTitle: {
-        type: String,
-        required: [true, "Job Title is required"],
-    },
-    jobType: {
-        type: String,
-        required: [true, "Job Type is required"],
-    },
-    location: {
-        type: String,
-        required: [true, "Location is required"],
-    },
-    salary: {
-        type: Number,
-        required: [true, "Salary is required"],
-    },
-    vacancies: { type: Number },
-    experience: {
-        type: Number,
-        default: 0
-    },
-    detail: [{
-        desc: { type: String, },
-        requirements: { type: String, }
-    }],
-    application: [
-        { type: Schema.Types.ObjectId, ref: "Users" }
-    ],
-
-},
     { timestamps: true }
 );
-
 
 const Jobs = mongoose.model("Jobs", jobSchema);
 
